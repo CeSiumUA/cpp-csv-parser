aee537121061befc9792efb0b17a61057ff00071
Fedir Katushonok <fedir.katushonok@gmail.com>
2022-08-30 23:27:06 +0300

    Added settings class

---
+++ settings.h
@@ -0,0 +1,15 @@
+#include <string>
+
+#ifndef CPP_CSV_SETTINGS_H
+#define CPP_CSV_SETTINGS_H
+
+using std::string;
+
+class settings {
+public:
+    string delimiter;
+
+};
+
+
+#endif //CPP_CSV_SETTINGS_H
